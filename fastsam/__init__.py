d24dad9786bdf8f1f0934e984a22b56f59491683
Ismail Baris <ismail.baris@dlr.de>
2025-03-01 16:00:07 +0100

    feat(Model): Add mlflow Model to init

--- fastsam/__init__.py
+++ fastsam/__init__.py
@@ -1,9 +1,127 @@
+# flake8: noqa
 # Ultralytics YOLO 🚀, AGPL-3.0 license
 
+# from .val import FastSAMValidator
+import warnings
+
+import numpy as np
+from mlflow.models.signature import ModelSignature
+from mlflow.types.schema import Schema, TensorSpec
+from mlops.trainer import BasePythonModel, MLFlowLightningModule
+
+from .decoder import FastSAMDecoder
 from .model import FastSAM
 from .predict import FastSAMPredictor
 from .prompt import FastSAMPrompt
-# from .val import FastSAMValidator
-from .decoder import FastSAMDecoder
 
-__all__ = 'FastSAMPredictor', 'FastSAM', 'FastSAMPrompt', 'FastSAMDecoder'
+__all__ = (
+    "FastSAMPredictor",
+    "FastSAM",
+    "FastSAMPrompt",
+    "FastSAMDecoder",
+    "FastSAMModel",
+)
+
+
+class FastSAMWrapper(BasePythonModel):
+    def load_context(self, context):
+        # Initialize the model with the stored parameters
+        self.state.pop("ckpt_path", None)
+
+        self.model = self.model_class(
+            ckpt_path=context.artifacts["checkpoint"],
+        )
+
+        self.model.eval()
+
+    def predict(self, context, model_input) -> np.ndarray:
+
+        # Input ============================================================================
+        # Extract patch, height, and width from the input dictionary
+        patch = model_input["patch"]  # Input Patch
+
+        # Make Prediction ==================================================================
+        # Make predictions with the model
+        with warnings.catch_warnings():
+            warnings.simplefilter("ignore")
+
+            output = self.model.predict(patch)
+
+        return output
+
+    def get_signature(self, **kwargs):
+
+        # Define input schema (considering patch as an object and height, width as integers)
+        input_schema = Schema(
+            [
+                TensorSpec(
+                    np.dtype(np.uint8), (-1, -1, 3), name="patch"
+                ),  # Dynamic H and W
+            ]
+        )
+
+        # Define output schema (assuming the output is a tensor or numerical value)
+        output_schema = Schema(
+            [TensorSpec(np.dtype(np.float32), (-1, -1, 3), name="output")]
+        )  # You can adjust this based on your model output
+
+        # Create the signature
+        signature = ModelSignature(inputs=input_schema, outputs=output_schema)
+
+        return signature
+
+
+class FastSAMModel(MLFlowLightningModule):
+    def __init__(
+        self,
+        ckpt_path=None,
+        retina_masks: bool = True,
+        imgsz: int = 512,
+        conf: float = 0.15,
+        iou: float = 0.95,
+    ):
+
+        super().__init__()
+        # Utility ==========================================================================
+        self.set_state(**locals())
+        self.name = "FastSamX"
+        self.wrapper = FastSAMWrapper
+
+        # Build Model ======================================================================
+        self.model = None
+        if ckpt_path is not None:
+            self.model = FastSAM(ckpt_path)
+
+        self.retina_masks = retina_masks
+        self.imgsz = imgsz
+        self.conf = conf
+        self.iou = iou
+        self.ckpt_path = ckpt_path
+
+    def predict(self, x: np.array):
+
+        everything_results = self.model(
+            x,
+            device=self.device,
+            retina_masks=self.retina_masks,
+            imgsz=self.imgsz,
+            conf=self.conf,
+            iou=self.iou,
+            verbose=False,
+        )
+
+        prompt_process = FastSAMPrompt(x, everything_results, device=self.device)
+        ann = prompt_process.everything_prompt()
+
+        if len(ann) > 0:
+            result = prompt_process.to_mask(
+                annotations=ann,
+                mask_random_color=True,
+                better_quality=True,
+                retina=True,
+            )
+
+        else:
+            result = np.zeros(x.shape)
+
+        return result
--- fastsam/model.py
+++ fastsam/model.py
@@ -14,8 +14,8 @@ from ultralytics.yolo.engine.exporter import Exporter
 from ultralytics.yolo.engine.model import YOLO
 from ultralytics.yolo.utils import DEFAULT_CFG, LOGGER, ROOT, is_git_dir
 from ultralytics.yolo.utils.checks import check_imgsz
-
 from ultralytics.yolo.utils.torch_utils import model_info, smart_inference_mode
+
 from .predict import FastSAMPredictor
 
 
@@ -37,14 +37,20 @@ class FastSAM(YOLO):
             (List[ultralytics.yolo.engine.results.Results]): The prediction results.
         """
         if source is None:
-            source = ROOT / 'assets' if is_git_dir() else 'https://ultralytics.com/images/bus.jpg'
+            source = (
+                ROOT / "assets"
+                if is_git_dir()
+                else "https://ultralytics.com/images/bus.jpg"
+            )
             LOGGER.warning(f"WARNING ⚠️ 'source' is missing. Using 'source={source}'.")
         overrides = self.overrides.copy()
-        overrides['conf'] = 0.25
+        overrides["conf"] = 0.25
         overrides.update(kwargs)  # prefer kwargs
-        overrides['mode'] = kwargs.get('mode', 'predict')
-        assert overrides['mode'] in ['track', 'predict']
-        overrides['save'] = kwargs.get('save', False)  # do not save by default if called in Python
+        overrides["mode"] = kwargs.get("mode", "predict")
+        assert overrides["mode"] in ["track", "predict"]
+        overrides["save"] = kwargs.get(
+            "save", False
+        )  # do not save by default if called in Python
         self.predictor = FastSAMPredictor(overrides=overrides)
         self.predictor.setup_model(model=self.model, verbose=False)
         try:
@@ -58,7 +64,7 @@ class FastSAM(YOLO):
 
     def val(self, **kwargs):
         """Run validation given dataset."""
-        overrides = dict(task='segment', mode='val')
+        overrides = dict(task="segment", mode="val")
         overrides.update(kwargs)  # prefer kwargs
         args = get_cfg(cfg=DEFAULT_CFG, overrides=overrides)
         args.imgsz = check_imgsz(args.imgsz, max_dim=1)
@@ -75,13 +81,15 @@ class FastSAM(YOLO):
         Args:
             **kwargs : Any other args accepted by the predictors. To see all args check 'configuration' section in docs
         """
-        overrides = dict(task='detect')
+        overrides = dict(task="detect")
         overrides.update(kwargs)
-        overrides['mode'] = 'export'
+        overrides["mode"] = "export"
         args = get_cfg(cfg=DEFAULT_CFG, overrides=overrides)
         args.task = self.task
         if args.imgsz == DEFAULT_CFG.imgsz:
-            args.imgsz = self.model.args['imgsz']  # use trained imgsz unless custom value is passed
+            args.imgsz = self.model.args[
+                "imgsz"
+            ]  # use trained imgsz unless custom value is passed
         if args.batch == DEFAULT_CFG.batch:
             args.batch = 1  # default to 1 if not modified
         return Exporter(overrides=args)(model=self.model)
@@ -103,4 +111,6 @@ class FastSAM(YOLO):
     def __getattr__(self, attr):
         """Raises error if object has no requested attribute."""
         name = self.__class__.__name__
-        raise AttributeError(f"'{name}' object has no attribute '{attr}'. See valid attributes below.\n{self.__doc__}")
+        raise AttributeError(
+            f"'{name}' object has no attribute '{attr}'. See valid attributes below.\n{self.__doc__}"
+        )
